be19c3918684d466fffc5554cbc2b9fef7e8932f
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2018-07-11 10:18:35 -0700

    add fixes for homework

--- link-checker/test/visit_test.js
+++ link-checker/test/visit_test.js
@@ -7,18 +7,21 @@ describe('visit', function () {
     it('should work for a single page', function () {
         const { input, expected } = helpers.singlePage();
         const sitemap = visit(input);
-        sitemap.forEach(page => expect(expected).to.contain(page));
+        expect(sitemap).to.have.lengthOf(expected.length);
+        sitemap.forEach(page => expect(expected).to.deep.contain(page));
     });
 
     it('should work with multiple pages', function () {
         const { input, expected } = helpers.multiplePages();
         const sitemap = visit(input);
-        sitemap.forEach(page => expect(expected).to.contain(page));
+        expect(sitemap).to.have.lengthOf(expected.length);
+        sitemap.forEach(page => expect(expected).to.deep.contain(page));
     });
 
     it('should work with pages that have nested links', function () {
         const { input, expected } = helpers.pagesWithLinks();
         const sitemap = visit(input);
-        sitemap.forEach(page => expect(expected).to.contain(page));
+        expect(sitemap).to.have.lengthOf(expected.length);
+        sitemap.forEach(page => expect(expected).to.deep.contain(page));
     });
 });
