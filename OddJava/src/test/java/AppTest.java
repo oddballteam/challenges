cb2a6c5d2d9cc4ebaf396c8bc7a60a9068965eb4
Chris Evardo <chris.evardo@oddball.io>
2020-04-10 09:46:47 -0400

    Clean up

--- OddJava/src/test/java/AppTest.java
+++ OddJava/src/test/java/AppTest.java
@@ -1,12 +1,11 @@
-package OddChallenge;
-
 import org.junit.Test;
 import static org.junit.Assert.*;
+import OddJava.App;
 
 public class AppTest {
     @Test
     public void testAppHasATest() {
-        App classUnderTest = new App();
+        App classUnderTest = new OddJava.App();
         assertNotNull("app should have tests", classUnderTest.getPatient());
     }
 }
